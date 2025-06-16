1b9f95593907593260c5fb4ebb284083b80cc279
Rsanch40 <Rsanch40@uic.edu>
2025-06-16 10:59:36 -0500

    Updated QTS Project Card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,11 @@ import React, { useEffect, useRef } from 'react';
 import ProjectCard from './ProjectCard';
 
 // Import images
-import image1 from '../images/OutTherePic.png';
-import image2 from '../images/learningBookPic.png';
-import image3 from '../images/ESApic.png';
-import image4 from '../images/portfolioWebPic.png';
+import image1 from '../images/QueueTheSong.png';
+import image2 from '../images/OutTherePic.png';
+import image4 from '../images/learningBookPic.png';
+import image5 from '../images/ESApic.png';
+import image3 from '../images/portfolioWebPic.png';
 //import image5 from '../images/learningBookPic.png';
 
 function Projects() {
@@ -39,11 +40,11 @@ function Projects() {
   }, []);
   
   const projects = [
-    { id: 1, title: 'OutThere', language: 'Java, JavaFX', description: 'A small event finder, for individuals to advertise street performances, art events, small concerts, and food events', image: image1, link: 'https://github.com/julianmackenzie/SparkHacks2023' },
-    { id: 2, title: 'LearningBook', language: 'HTML, CSS, JavaScript', description: 'A web prototype for a mobile app which aims to improve English proficiency through reading, vocabulary, audio, and translation', image: image2, link: 'https://github.com/Rsanch40/CS-422-Project' },
-    { id: 3, title: 'Endangered Species Analysis', language: 'Python, Pandas', description: 'A data analysis project that looks into endangered species and the cause of their endangerment. Includes analysis from Machine Learning techniques such as Neural Networks', image: image3, link: 'https://github.com/Rsanch40/Endangered-Species-Data-Project.git'},
-    { id: 4, title: 'Portfolio Website', language: 'React.js, Node.js, HTML, CSS, Firebase', description: 'This Website!', image: image4, link: 'https://github.com/Rsanch40/MyPortfolioWebsite' },
-    { id: 5, title: '[IN PROGRESS] Queue The Song - A Spotify App', language: 'React.js, Node.js, Tailwind CSS, AWS, Spotify Web API', description: 'A Web App using Spotify API to display currently-playing song, user queue, and allow users to automatically queue songs when conditions are met', link: 'https://github.com/Rsanch40/Queue-The-Song'},
+    { id: 1, title: 'Queue The Song - A Spotify App', language: 'React.js, Node.js, Tailwind CSS, AWS, Spotify Web API', description: 'A Web App using Spotify API to display currently-playing song, user queue, and allow users to automatically queue songs when conditions are met', image: image1, link: 'https://github.com/Rsanch40/Queue-The-Song'},
+    { id: 2, title: 'OutThere', language: 'Java, JavaFX', description: 'A small event finder, for individuals to advertise street performances, art events, small concerts, and food events', image: image2, link: 'https://github.com/julianmackenzie/SparkHacks2023' },
+    { id: 3, title: 'Portfolio Website', language: 'React.js, Node.js, HTML, CSS, Firebase', description: 'This Website!', image: image3, link: 'https://github.com/Rsanch40/MyPortfolioWebsite' },
+    { id: 4, title: 'LearningBook', language: 'HTML, CSS, JavaScript', description: 'A web prototype for a mobile app which aims to improve English proficiency through reading, vocabulary, audio, and translation', image: image4, link: 'https://github.com/Rsanch40/CS-422-Project' },
+    { id: 5, title: 'Endangered Species Analysis', language: 'Python, Pandas', description: 'A data analysis project that looks into endangered species and the cause of their endangerment. Includes analysis from Machine Learning techniques such as Neural Networks', image: image5, link: 'https://github.com/Rsanch40/Endangered-Species-Data-Project.git'},
   ];
 
   return (
